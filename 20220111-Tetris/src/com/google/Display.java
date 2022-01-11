ca9044a63f9bd82f76b63dd118165771cc4ce16a
jannik-boeppli <jannik.boeppli@noseryoung.ch>
2022-01-11 11:39:40 +0100

    Added a user input handler

--- 20220111-Tetris/src/com/google/Display.java
+++ 20220111-Tetris/src/com/google/Display.java
@@ -1,31 +1,40 @@
 package com.google;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.concurrent.BlockingQueue;
 
 public class Display extends JFrame implements KeyListener {
-    public Display()  {
+    private BlockingQueue<Integer> userInput;
+
+    public Display(BlockingQueue<Integer> userInput) {
         setDefaultValues();
+        this.userInput = userInput;
     }
 
-    public void setDefaultValues(){
+    public void setDefaultValues() {
         this.setTitle("Tetris - Andrin, Jannik");
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.requestFocusInWindow();
+        addKeyListener(this);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        try {
+            userInput.put(e.getKeyCode());
+        } catch (InterruptedException ignored) {}
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+    }
 }
--- 20220111-Tetris/src/com/google/Manager.java
+++ 20220111-Tetris/src/com/google/Manager.java
@@ -1,7 +1,23 @@
 package com.google;
 
+import java.awt.event.MouseListener;
+
 public class Manager {
+    private MoveController moveController;
+    private UserInput userInput;
+    private AutoLower autoLower;
+    private Display display;
+
     public Manager() {
-        new Display();
+        moveController = new MoveController();
+        moveController.start();
+
+        userInput = new UserInput(moveController.getNextMove());
+        userInput.start();
+
+        autoLower = new AutoLower(moveController.getNextMove());
+        autoLower.start();
+
+        display = new Display(userInput.getUserInput());
     }
 }
