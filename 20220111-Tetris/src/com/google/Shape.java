f3e28e7e0a9f54d55c5b372f505e48214c859e1f
andrinklarer <andrin.klarer@noseryoung.ch>
2022-01-29 22:08:26 +0100

    Update Shapes

--- 20220111-Tetris/src/com/google/Shape.java
+++ 20220111-Tetris/src/com/google/Shape.java
@@ -8,8 +8,56 @@ public class Shape {
     String desc;
 
 
-    public Shape(int[][] shape, String desc) {
+    public Shape(int[][] shape, String desc, Color color) {
         this.shape = shape;
         this.desc = desc;
+        this.color = color;
+    }
+
+    public Shape() {
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int[][] getShape() {
+        return shape;
+    }
+
+    public void setShape(int[][] shape) {
+        this.shape = shape;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setX(int index, int x) {
+
+        this.shape[index][0] = x;
+    }
+
+    public void setY(int index, int y) {
+
+        this.shape[index][1] = y;
+    }
+
+    int x(int index, Shape shape) {
+
+        return shape.getShape()[index][0];
+    }
+
+    int y(int index, Shape shape) {
+
+        return shape.getShape()[index][1];
     }
 }
--- 20220111-Tetris/src/com/google/ShapeManager.java
+++ 20220111-Tetris/src/com/google/ShapeManager.java
@@ -1,5 +1,6 @@
 package com.google;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -22,22 +23,43 @@ public class ShapeManager {
     };
 
     public void initShapes(){
-        Shape shape1 = new Shape(getShape(0), "Empty" );
-        Shape shape2 = new Shape(getShape(1), "Square" );
-        Shape shape3 = new Shape(getShape(2), "Line" );
-        Shape shape4 = new Shape(getShape(3), "Mirrored L" );
-        Shape shape5 = new Shape(getShape(4), "L" );
-        Shape shape6 = new Shape(getShape(5), "Z" );
-        Shape shape7 = new Shape(getShape(6), "S" );
-        Shape shape8 = new Shape(getShape(7), "Mirrored T" );
+        Shape shape1 = new Shape(getShapeCoords(0), "Empty", Color.BLACK );
+        Shape shape2 = new Shape(getShapeCoords(1), "Square", Color.YELLOW );
+        Shape shape3 = new Shape(getShapeCoords(2), "Line", Color.BLUE );
+        Shape shape4 = new Shape(getShapeCoords(3), "Mirrored L", Color.RED );
+        Shape shape5 = new Shape(getShapeCoords(4), "L", Color.PINK);
+        Shape shape6 = new Shape(getShapeCoords(5), "Z", Color.GREEN);
+        Shape shape7 = new Shape(getShapeCoords(6), "S", Color.CYAN);
+        Shape shape8 = new Shape(getShapeCoords(7), "Mirrored T", Color.CYAN );
         shapeList.addAll(Arrays.asList(shape1,shape2,shape3,shape4,shape5,shape6,shape7,shape8));
     }
 
-    public int[][] getShape(int index){
+    public int[][] getShapeCoords(int index){
         int[][] x = new int[4][2];
         for (int i = 0; i < 4; i++){
             System.arraycopy(shapes[index][i], 0, x[i], 0, 2);
         }
-        return new int[][] {{x[0][0],x[0][0],},{x[1][0],x[1][1],},{x[2][0],x[2][1],},{x[3][0],x[3][1],}};
+        return new int[][] {{x[0][0],x[0][1],},{x[1][0],x[1][1],},{x[2][0],x[2][1],},{x[3][0],x[3][1],}};
+    }
+
+
+
+
+    public Shape rotateLeft(Shape shape){
+        if (shape.getDesc().equals("Square")){
+            return shapeList.get(0);
+        }
+
+        Shape turned = new Shape();
+        turned.setShape(shape.getShape());
+        turned.setColor(shape.getColor());
+        turned.setDesc(shape.getDesc());
+
+        for (int i = 0; i < 4; i++) {
+            turned.setX(i, shape.y(i, shape));
+            turned.setY(i, -shape.x(i, shape));
+        }
+        return turned;
     }
 }
+
